b010273a2095efb522f7d2f5bf0704e0cc0cc863
davidegaspar <git@davidegaspar.com>
2019-01-07 17:39:02 +0000

    wip

--- backup/backup.sh
+++ backup/backup.sh
@@ -4,7 +4,7 @@ export AWS_DEFAULT_REGION="eu-west-1"
 
 ## CLEAN OLD SNAPSHOTS
 # get all circleci images
-aws ec2 describe-images --filters "Name=tag:service,Values=circleci" > circleci_services_images.json
+# aws ec2 describe-images --filters "Name=tag:service,Values=circleci" > circleci_services_images.json
 # get list of the oldest if more than X
 # deregister images
 # IMAGE_ID="ami-06a70aa2a2f70c764"
@@ -21,5 +21,5 @@ NAME="circleci-services-automated-test-$(date -u +%Y%m%dT%H%M%SZ)"
 # create image with name
 # aws ec2 create-image --no-reboot --instance-id $INSTANCE_ID --name $NAME
 # tag image
-IMAGE_ID="ami-0bbf4e2c092bc60c9"
-# aws ec2 create-tags --resources $IMAGE_ID --tags "Key=service,Value=circleci"
+IMAGE_ID="ami-0212188ddf059e6e2"
+aws ec2 create-tags --resources $IMAGE_ID --tags "Key=service,Value=circleci"
